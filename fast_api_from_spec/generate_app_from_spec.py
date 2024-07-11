764747ef00d34f9d86e750bc4cb68d73eafdc8fb
MohamedMahameed <mohamed.mahameed@ibm.com>
2024-07-11 16:19:04 +0300

    remove pydentic and return simple JSON response

--- fast_api_from_spec/generate_app_from_spec.py
+++ fast_api_from_spec/generate_app_from_spec.py
@@ -1,13 +1,10 @@
-from enum import Enum
-from fastapi import FastAPI
-from pydantic import BaseModel
 from get_openapi_paths import load_openapi_spec,parse_endpoints
-import yaml
+import json
 from jinja2 import Environment, FileSystemLoader
 
 
 def render_fastapi_template(output_file, endpoints, components):
-    env = Environment(loader=FileSystemLoader('.'))
+    env = Environment(loader=FileSystemLoader('.'), extensions=['jinja2.ext.loopcontrols'])
     template = env.get_template('fast_api_template.jinja2')
     data = {
         "endpoints": endpoints,
@@ -23,7 +20,6 @@ def render_fastapi_template(output_file, endpoints, components):
 
 if __name__ == "__main__":
     yaml_file_path = 'fdp-amts-gtfs-static.yaml'
-    #endpoints = get_openapi_paths(yaml_file_path)
     openapi_spec = load_openapi_spec(yaml_file_path)
     endpoints = parse_endpoints(openapi_spec)
     components = openapi_spec.get("components", {}).get("schemas", {})
--- fast_api_from_spec/generated_fastapi_app.py
+++ fast_api_from_spec/generated_fastapi_app.py
@@ -1,70 +1,35 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional, List, Any
 import requests
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-# Define Pydantic models for the components
-
-
-class Stop(BaseModel):
-        stop_id: int
-        stop_name: str
-        stop_lat: float
-        stop_lon: float
-        location_type: int
-        parent_station: str
-        wheelchair_boarding: str
-
-class Stops(BaseModel):
-        items: List[Stop]
-
-
 # Define FastAPI endpoints
 
-@app.get("/stops", response_model=Stops)
+@app.get("/stops")
 async def getStops():
     """
     Returns all the stops that are present in the FDP.
 
     """
     path_params = {
-        
     }
     query_params = {
-        
     }
     response = requests.get("http://mobility.teadal.ubiwhere.com/fdp-amts-gtfs-static/stops".format(**path_params), params=query_params)
     response_data = response.json()
-    return Stops(
-        type=response_data["type"],
-        items=response_data["items"])
-    
+    return JSONResponse(response_data)
 
-
-@app.get("/stops/stop_id/{stop_id}", response_model=Stop)
+@app.get("/stops/stop_id/{stop_id}")
 async def getStopById(stop_id: str):
     """
     Returns the data of a stop given its unique identifier.
     """
     path_params = {
-        
         "stop_id": stop_id
-        
     }
     query_params = {
-        
     }
     response = requests.get("http://mobility.teadal.ubiwhere.com/fdp-amts-gtfs-static/stops/stop_id/{stop_id}".format(**path_params), params=query_params)
     response_data = response.json()
-    return Stop(
-        stop_id=response_data["stop_id"],
-        stop_name=response_data["stop_name"],
-        stop_lat=response_data["stop_lat"],
-        stop_lon=response_data["stop_lon"],
-        location_type=response_data["location_type"],
-        parent_station=response_data["parent_station"],
-        wheelchair_boarding=response_data["wheelchair_boarding"])
-    
-
+    return JSONResponse(response_data)
